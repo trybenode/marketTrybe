8381581a4180017afb5494dff41af559930616f5
Ezekiel Eyitayo <ezekieleyitayo2020@gmail.com>
2025-03-05 08:26:55 +0100

    Deleted a useless file

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/screens/LoginScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
-import HomeScreen from './src/screens/HomeScreen';
+// import HomeScreen from './src/screens/HomeScreen';
 import './global.css';
 
 const Stack = createNativeStackNavigator();
