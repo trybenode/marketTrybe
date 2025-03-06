7b0672fd6e88567ef69794980b53f013ca1cbddb
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-03-06 07:17:28 +0100

    created dummy data and added image

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,71 @@
 import { View, Text, FlatList, TouchableOpacity, Image, Button } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 
-const dummyCategories = [
-  { id: '1', name: 'Electronics', image: '' },
-  { id: '2', name: 'Clothing' },
-  { id: '3', name: 'Home Appliances' },
+const categories = [
+  {
+    id: '1',
+    name: 'Electronics',
+    image:
+      'https://images.pexels.com/photos/28771662/pexels-photo-28771662/free-photo-of-vintage-electronics-and-media-shelf-display.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
+  {
+    id: '2',
+    name: 'Clothing',
+    image: 'https://www.pexels.com/photo/low-angle-view-of-shoes-322207/',
+  },
+  {
+    id: '3',
+    name: 'Home Appliances',
+    image:
+      'https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
+  {
+    id: '4',
+    name: 'Foot Wear ',
+    image:
+      'https://images.pexels.com/photos/6153367/pexels-photo-6153367.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
+  {
+    id: '5',
+    name: 'Jewelry',
+    image:
+      'https://images.pexels.com/photos/1616096/pexels-photo-1616096.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
+  {
+    id: '6',
+    name: 'Hair Accesories ',
+    image:
+      'https://images.pexels.com/photos/30988728/pexels-photo-30988728/free-photo-of-elegant-flat-lay-of-beauty-accessories-on-magazine.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
+  {
+    id: '7',
+    name: 'Body Care',
+    image:
+      'https://images.pexels.com/photos/4202325/pexels-photo-4202325.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
+  {
+    id: '8',
+    name: 'Snacks',
+    image:
+      'https://images.pexels.com/photos/30957062/pexels-photo-30957062/free-photo-of-stack-of-delicious-homemade-chocolate-cookies.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
 ];
+
+// useEffect(() => {
+//   const fetchCategories = async () => {
+//     try {
+//       const querySnapshot = await getDocs(collection(db, 'categories'));
+//       const categoryList = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+//       setCategories(categoryList.length > 0 ? categoryList.slice(0, 6) : dummyCategories);
+//     } catch (error) {
+//       console.error('Error fetching categories:', error);
+//     }
+//   };
+//   fetchCategories();
+// }, []);
 const Categories = () => {
-  const [categories, setCategories] = useState(dummyCategories);
+  //   const [categories, setCategories] = useState(dummyCategories);
   const navigation = useNavigation();
   return (
     <View>
