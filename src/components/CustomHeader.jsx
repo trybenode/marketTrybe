193c5e85c692cd631ac0cab355be9ac4c9086c73
Ezekiel Eyitayo <ezekieleyitayo2020@gmail.com>
2025-03-10 14:05:26 +0100

    Created the Custom Header Component

---
+++ src/components/CustomHeader.jsx
@@ -0,0 +1,20 @@
+import { useNavigation } from '@react-navigation/native'; // ✅ Correct way
+import React, { memo } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const CustomHeader = memo(({ screenName, title, extraComponent }) => {
+  const navigation = useNavigation(); // ✅
+
+  return (
+    <View className="mb-5 mt-8 flex-row items-center justify-between p-1">
+      <TouchableOpacity onPress={() => navigation.navigate(screenName)}>
+        <Ionicons name="arrow-back" size={30} color="black" />
+      </TouchableOpacity>
+      <Text className="text-xl font-bold">{title}</Text>
+      {extraComponent || <View />}
+    </View>
+  );
+});
+
+export default CustomHeader;
