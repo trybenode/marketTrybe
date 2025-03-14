a64928e35160092f560d296584af13f68bf80783
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-03-14 11:48:06 +0100

    dynamically rendered button and updated data

--- src/screens/SellScreen.jsx
+++ src/screens/SellScreen.jsx
@@ -1,12 +1,12 @@
+import { useNavigation } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
-import { TouchableOpacity, TextInput, View, Text, ScrollView, Alert} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import BackButton from '../components/BackButton';
+import { TouchableOpacity, TextInput, View, Text, ScrollView, Alert, Button } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
-import UploadImages from '../components/UploadImages';
 import { Checkbox } from 'react-native-paper';
-// import TestHeader from '../components/TestHeader';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 import CustomHeader from '../components/CustomHeader';
+import UploadImages from '../components/UploadImages';
 
 export default function SellScreen({ route }) {
   const [productName, setProductName] = useState('');
@@ -35,16 +35,19 @@ export default function SellScreen({ route }) {
 
   const isEditMode = route.params?.product !== undefined;
   const product = route.params?.product;
+  const navigation = useNavigation();
 
-  //prefill form if product exist
+  // Prefill form if product exists
   useEffect(() => {
-    if (isEditMode) {
+    if (isEditMode && product) {
       setProductName(product.name);
       setSubCategory(product.subcategory);
       setSelectedValue(product.selectedValue);
-      setCategory(product.category);
+      // Ensure category is always an array
+      if (Array.isArray(product.category)) {
+        setCategory(product.category);
+      }
       setIsNegotiable(product.negotiable);
-      // setIsAgreed(product.agreed);
       setProductDescription(product.description);
       setBrand(product.brand);
       setCondition(product.condition);
@@ -55,10 +58,34 @@ export default function SellScreen({ route }) {
   }, [isEditMode, product]);
 
   const handleSubmit = () => {
-    if (!productName || !price || category || isNegotiable || productDescription || brand || condition || color || year ) {
+    if (
+      !productName ||
+      !price ||
+      !selectedValue ||
+      !productDescription ||
+      !brand ||
+      !condition ||
+      !color ||
+      !year
+    ) {
       Alert.alert('Error', 'Please fill in all fields');
       return;
+    }
+
+    const productData = {
+      name: productName,
+      subcategory: subCategory,
+      selectedValue,
+      category,
+      negotiable: isNegotiable,
+      description: productDescription,
+      brand,
+      condition,
+      color,
+      price,
+      year,
     };
+
     if (isEditMode) {
       // Handle product update logic
       console.log('Updating Product:', productData);
@@ -72,123 +99,116 @@ export default function SellScreen({ route }) {
     // Navigate back after submission
     navigation.goBack();
   };
-    
-  
 
-    return (
-      <SafeAreaView className="mb-10 flex-1 bg-white">
-        {/* <BackButton screenName="MainTabs" /> */}
-        <CustomHeader screenName="MainTabs" />
+  return (
+    <SafeAreaView className="mb-20 flex-1 bg-white">
+      <CustomHeader screenName="MainTabs" title="Product Information" />
 
-        <View className="mt-1 flex-1 px-4">
-          <ScrollView
-            contentContainerStyle={{ flexGrow: 1 }}
-            showsVerticalScrollIndicator={false}
-            className="px-2">
-            <Text className="mb-6 text-center text-lg font-bold">Product Information</Text>
-            <TextInput
-              className="mb-4 rounded border bg-white p-4"
-              placeholder="Product Name"
-              value={productName}
-              onChangeText={setProductName}
-            />
-            <DropDownPicker
-              open={open}
-              value={selectedValue}
-              items={category}
-              setOpen={setOpen}
-              setValue={setSelectedValue}
-              setItems={setCategory}
-              placeholder="Select Category"
-              style={{ marginBottom: 10 }}
-            />
-            <TextInput
-              className="mb-4 rounded border bg-white p-4"
-              placeholder="Sub-Category"
-              value={subCategory}
-              onChangeText={setSubCategory}
-            />
+      <View className="mt-1 flex-1 px-4">
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          showsVerticalScrollIndicator={false}
+          className="px-2">
+          <TextInput
+            className="mb-4 rounded border bg-white p-4"
+            placeholder="Product Name"
+            value={productName}
+            onChangeText={setProductName}
+          />
+          <DropDownPicker
+            open={open}
+            value={selectedValue}
+            items={category}
+            setOpen={setOpen}
+            setValue={setSelectedValue}
+            setItems={setCategory}
+            placeholder="Select Category"
+            style={{ marginBottom: 10 }}
+          />
+          <TextInput
+            className="mb-4 rounded border bg-white p-4"
+            placeholder="Sub-Category"
+            value={subCategory}
+            onChangeText={setSubCategory}
+          />
 
-            <View className="flex-row items-center justify-between">
-              <View className="flex-row items-center">
-                <Checkbox
-                  status={isNegotiable ? 'checked' : 'unchecked'}
-                  onPress={() => setIsNegotiable(!isNegotiable)}
-                  className="border "
-                  color="#6200EE"
-                />
-                <Text className="ml-2 text-lg">Negotiable</Text>
-              </View>
-              <UploadImages />
-            </View>
-
-            <TextInput
-              className="mb-4 h-32 rounded border bg-white p-4"
-              placeholder="Product Description"
-              value={productDescription}
-              onChangeText={setProductDescription}
-              multiline
-              textAlignVertical="top"
-            />
-
-            <View className="mb-4">
-              <Text className="mb-4 text-center font-semibold">Other Information</Text>
-              <TextInput
-                className="mb-4 rounded border bg-white p-4"
-                placeholder="Brand/Type/Model"
-                value={brand}
-                onChangeText={setBrand}
-              />
-              <View className="flex-row justify-between">
-                <TextInput
-                  className="mb-4 w-[48%] rounded border bg-white p-4"
-                  placeholder="Condition (New/Used)"
-                  value={condition}
-                  onChangeText={setCondition}
-                />
-                <TextInput
-                  className="mb-4 w-[48%] rounded border bg-white p-4"
-                  placeholder="Color"
-                  value={color}
-                  onChangeText={setColor}
-                />
-              </View>
-              <View className="flex-row justify-between">
-                <TextInput
-                  className="mb-4 w-[48%] rounded border bg-white p-4"
-                  placeholder="Price (N)"
-                  keyboardType="numeric"
-                  value={price}
-                  onChangeText={setPrice}
-                />
-                <TextInput
-                  className="mb-4 w-[48%] rounded border bg-white p-4"
-                  placeholder="Year"
-                  keyboardType="numeric"
-                  value={year}
-                  onChangeText={setYear}
-                />
-              </View>
-            </View>
-
-            <View className="mb-4 flex-row items-center">
+          <View className="flex-row items-center justify-between">
+            <View className="flex-row items-center">
               <Checkbox
-                status={isAgreed ? 'checked' : 'unchecked'}
-                onPress={() => setIsAgreed(!isAgreed)}
-                className="border"
+                status={isNegotiable ? 'checked' : 'unchecked'}
+                onPress={() => setIsNegotiable(!isNegotiable)}
                 color="#6200EE"
               />
-              <Text className="ml-2 text-lg">I accept the Terms & Conditions</Text>
+              <Text className="ml-2 text-lg">Negotiable</Text>
             </View>
+            <UploadImages />
+          </View>
 
-            <TouchableOpacity
-              disabled={!isAgreed}
-              className={`mx-auto mb-24 w-[60%] rounded-lg p-3 ${isAgreed ? 'bg-blue-500' : 'bg-gray-400'}`}>
-              <Text className="text-center font-bold text-white">Submit</Text>
-            </TouchableOpacity>
-          </ScrollView>
-        </View>
-      </SafeAreaView>
-    );
-  };
+          <TextInput
+            className="mb-4 h-32 rounded border bg-white p-4"
+            placeholder="Product Description"
+            value={productDescription}
+            onChangeText={setProductDescription}
+            multiline
+            textAlignVertical="top"
+          />
+
+          <View className="mb-4">
+            <Text className="mb-4 text-center font-semibold">Other Information</Text>
+            <TextInput
+              className="mb-4 rounded border bg-white p-4"
+              placeholder="Brand/Type/Model"
+              value={brand}
+              onChangeText={setBrand}
+            />
+            <View className="flex-row justify-between">
+              <TextInput
+                className="mb-4 w-[48%] rounded border bg-white p-4"
+                placeholder="Condition (New/Used)"
+                value={condition}
+                onChangeText={setCondition}
+              />
+              <TextInput
+                className="mb-4 w-[48%] rounded border bg-white p-4"
+                placeholder="Color"
+                value={color}
+                onChangeText={setColor}
+              />
+            </View>
+            <View className="flex-row justify-between">
+              <TextInput
+                className="mb-4 w-[48%] rounded border bg-white p-4"
+                placeholder="Price (N)"
+                keyboardType="numeric"
+                value={price}
+                onChangeText={setPrice}
+              />
+              <TextInput
+                className="mb-4 w-[48%] rounded border bg-white p-4"
+                placeholder="Year"
+                keyboardType="numeric"
+                value={year}
+                onChangeText={setYear}
+              />
+            </View>
+          </View>
+
+          <View className="mb-4 flex-row items-center">
+            <Checkbox
+              status={isAgreed ? 'checked' : 'unchecked'}
+              onPress={() => setIsAgreed(!isAgreed)}
+              color="#6200EE"
+            />
+            <Text className="ml-2 text-lg">I accept the Terms & Conditions</Text>
+          </View>
+
+          <Button
+            disabled={!isAgreed}
+            title={isEditMode ? 'Update Product' : 'Upload Product'}
+            onPress={handleSubmit}
+          />
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
 }
