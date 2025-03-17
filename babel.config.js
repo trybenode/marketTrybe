08f7305373555ee79d8ceed7b25c33ff0ff103f0
Allen Douglas <allenissabigboi@gmail.com>
2025-03-17 15:39:07 -0700

    add dotenv support for environment variables in babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = [];
+  const plugins = [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+  ];
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-
     plugins,
   };
 };
