b6cf8d662a62b75e38ac02ab429a47ef5214759f
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-04-02 09:26:06 +0100

    added comment to categories and clean it up

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -10,7 +10,6 @@ import CategoriesListing from './CategoriesListing';
 const CategoryList = memo(() => {
   const navigation = useNavigation();
   const [categories, setCategories] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   const shuffleArray = (array) => {
     return array.sort(() => Math.random() - 0.5);
@@ -26,12 +25,9 @@ const CategoryList = memo(() => {
         }));
 
         const shuffledCategories = shuffleArray([...categoryList]).slice(0, 6);
-        // Take only the first 6 categories
         setCategories(shuffledCategories);
       } catch (error) {
         console.error('Error fetching categories:', error);
-      } finally {
-        setLoading(false);
       }
     };
 
@@ -42,27 +38,21 @@ const CategoryList = memo(() => {
     <View className="mt-1">
       <View className="mx-1 mb-2 flex flex-row items-center justify-between">
         <Text className="w-1/3 text-xl font-semibold">Categories</Text>
-        <TouchableOpacity
+        <TouchableOpacity //button to see all categories
           onPress={() => navigation.navigate('Categories')}
           className="rounded bg-blue-500 px-3 py-2">
           <Text className="font-semibold text-white">See All</Text>
         </TouchableOpacity>
       </View>
 
-      {loading ? (
-        <View className="flex-1 items-center justify-center">
-          <ActivityIndicator size="large" color="#2563eb" />
-        </View>
-      ) : (
-        <FlatList
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          data={categories}
-          keyExtractor={(item) => item.id}
-          className="p-2"
-          renderItem={({ item }) => <CategoriesListing {...item} />}
-        />
-      )}
+      <FlatList //renders individual category
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={categories}
+        keyExtractor={(item) => item.id}
+        className="p-2"
+        renderItem={({ item }) => <CategoriesListing {...item} />}
+      />
     </View>
   );
 });
