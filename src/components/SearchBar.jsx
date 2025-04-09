c333233daf6b4c362962a9db9627cf104b11f93f
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-04-09 17:09:57 +0100

    implemented search functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,108 @@
 //search bar used on homepage
 
-import React, { useState, memo } from 'react';
-import { View, TextInput } from 'react-native';
+import React, { useState, useEffect, memo } from 'react';
+import { View, TextInput, StyleSheet,Keyboard, Activi } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useDebounce } from '../utils/useDebounce';
+import {collection, doc, getDocs} from 'firebase/firestore'
+import { db } from '../../firebaseConfig';
 
-const SearchBar = memo(() => {
-  const [searchQuery, setSearchQuery] = useState('');
+const SearchBar = ({ onResults}) => {
 
+  const [products, setProducts] = useState([]);
+  const [filtered,setFiltered] =useState([]);
+  const [searchQuery, setSearchQuery]=useState('');
+  const [loading, setLoading] = useState(true)
+  
+  const debounceQuery = useDebounce(searchQuery, 500)
+  
+  useEffect(()=>{
+    const fetchProducts = async ()=>{
+      setLoading(true)
+      const querySnapshot = await getDocs(collection(db, 'products'));
+      const data = querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data() }));
+      setProducts(data);
+      setFiltered(data);
+      setLoading(false);
+    }
+
+    fetchProducts();
+  },[])
+
+  useEffect(() =>{
+    if (!debounceQuery.trim() ){
+      setFiltered(products);
+      return;
+    }
+
+    const filteredData = products.filter(product => 
+      product.name.toLowerCase().includes(debounceQuery.toLowerCase()) ||
+      product.categoryId.toLowerCase().includes(debounceQuery.toLowerCase()) ||
+      product.brand.toLowerCase().includes(debounceQuery.toLowerCase()) ||
+      product.subcategory.toLowerCase().includes(debounceQuery.toLowerCase()) ||
+      (product.tags && product.tags.some(tag => tag.toLowerCase().includes(debounceQuery.toLowerCase())))
+
+    ).map((prod) => ({
+      id: prod.id,
+      product: {
+        ...prod,
+        createdAt: prod.createdAt,
+        updatedAt: prod.updatedAt,
+      },
+    }));
+  onResults(filteredData, false)
+    setFiltered(filteredData);
+  }, [debounceQuery,products])
+
+  useEffect(() => {
+    if (onResults) {
+      onResults(filtered, loading);
+    }
+  }, [filtered, loading]);
+
+  const handleClearSearch = () =>{
+    setSearchQuery('');
+    Keyboard.dismiss();
+    onResults([], false);
+  }
+ 
   return (
-    <View className="my-4 rounded-lg border border-gray-300 bg-white px-4 py-1">
+    <View style={styles.container}>
+      <Ionicons name="search" size={20} color="blue" />
       <TextInput
-        placeholder="Search..."
+        style={styles.input}
+        placeholder="Search products..."
         value={searchQuery}
         onChangeText={setSearchQuery}
-        className="text-base"
       />
+      {searchQuery.length > 0 && (
+          <Ionicons 
+          name="close-circle"
+          size={30}
+          color="blue"
+          onPress={handleClearSearch} // Clear search
+        />
+
+        )
+      }
     </View>
   );
-});
+};
 
-export default SearchBar;
+export default memo(SearchBar);
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: '#eee',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginBottom: 10,
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    marginLeft: 8,
+  },
+});
